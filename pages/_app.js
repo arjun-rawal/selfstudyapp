4befd9dee785df11418a02ea916d5e00e428d64e
arjun-rawal <arawalarjun@gmail.com>
2025-01-31 15:30:40 -0500

    vercel

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import "@/styles/globals.css";
 import { Provider } from "@/components/ui/provider";
-
+import { Analytics } from "@vercel/analytics/react"
 export default function App({ Component, pageProps }) {
   return (
     <Provider>
